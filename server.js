a056ac1bc0787abe68231019cf1bff9ebe503f10
Abhishek Pant <abhishekpant1990@gmail.com>
2022-12-30 23:11:12 +0530

    real-time-code-sync

--- server.js
+++ server.js
@@ -1,15 +1,90 @@
-const express = require('express')
+const express = require('express');
 const app=express();
+
 const http=require('http');
 
+
+//requiring Server class
 const {Server}=require('socket.io');
+const ACTIONS = require('./src/Actions');
+//creating http server
 
 const server=http.createServer(app);
+
+
+
+//creating instance of Server class to use its methods and attributes
 const io=new Server(server);
 
 
-io.on()
+//object to store user along with there socket id
+const userSocketMap={};
+
+
+function getAllConnectedClients(roomId)
+{
+  //Map of socketId : username
+  //adapter is used to retrieve information from socket
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId)=>{
+    return{
+      socketId,
+      username:userSocketMap[socketId], 
+    }
+  });
+}
+
+//connection event gets triggered whenever any sockets gets connected to our server
+io.on('connection',(socket)=>{
+  console.log('socket connected',socket.id);
+
+  //Listening to event emitted by client
+  socket.on(ACTIONS.JOIN,({roomId,username })=>{
+    userSocketMap[socket.id]=username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+
+    //Emitting the event (list of all joined clients) so that every client 
+    //can know how many people are joined
+    //we'll be listening this event in front-end
+    clients.forEach(({socketId})=>{
+      io.to(socketId).emit(ACTIONS.JOINED,{
+        clients,
+        username,
+        socketId:socket.id,
+      });  
+    });
+
+    console.log(clients);
+
+  });
+
+  //Receiving code change event from client and then we'll send this change to rest of clients 
+  //on() listener is used to listen and to() used to send 
+  socket.on(ACTIONS.CODE_CHANGE,({roomId,code})=>{
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE,{code});
+  });
+
+   socket.on(ACTIONS.SYNC_CODE,( {socketId,code})=>{
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE,{code});
+  });
+
+  //whenever some one leave room or closes browser tab browser will send disconnecting event to server
+  socket.on('disconnecting',()=>{
+
+    //getting all rooms in which disconnecting client is connected
+    const rooms=[...socket.rooms];
+    rooms.forEach((roomId) => { 
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+        socketId:socket.id,
+        username:userSocketMap[socket.id],
+      });
+    });
+
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
+});
 
 
 const PORT=process.env.PORT || 5000;
-server.listen(PORT,()=>console.log('Listening to port ${PORT}'));
+server.listen(PORT,()=>console.log(`Listening to port ${PORT}`));
--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useRef} from 'react';
 
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css'; 
@@ -6,18 +6,39 @@ import 'codemirror/theme/dracula.css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
+import { Socket } from 'socket.io-client';
+import ACTIONS from '../Actions';
+import userEvent from '@testing-library/user-event';
 
-const Editor = () => {
+const Editor = ({socketRef,roomId,onCodeChange}) => {
 
+  const editorRef=useRef(null);
   useEffect(()=>{
     async function init(){
         
-        Codemirror.fromTextArea(document.getElementById('realTimeEditor'),{
+      //used codemirror to convert our text area into editor
+        editorRef.current=Codemirror.fromTextArea(document.getElementById('realTimeEditor'),{
           mode:{name:'javascript',json: true},
           theme: 'dracula',
           autoCloseTags:true,
           autoCloseBrackets:true,
           lineNumbers:true,
+      });
+      //change is codemirror method
+      editorRef.current.on('change',(instance,changes)=>{
+        // console.log('changes',changes);
+        const {origin}=changes;
+        const code=instance.getValue();
+        onCodeChange(code);
+        console.log(code);
+        if(origin!=='setValue')
+        {
+          socketRef.current.emit(ACTIONS.CODE_CHANGE,{
+            roomId,
+            code,
+          });
+        }
+
       });
     }
 
@@ -25,7 +46,27 @@ const Editor = () => {
 
   }, []);
 
+  //changing code in real time by setting the value received from server
+  useEffect(()=>{
+
+    if(socketRef.current){
+
+      socketRef.current.on(ACTIONS.CODE_CHANGE,({code})=>{
+      if(code!=null)
+      {
+        editorRef.current.setValue(code); 
+      }
+
+      return ()=>{
+        socketRef.current.off(ACTIONS.CODE_CHANGE);
+      }
+    });
+  }
+
+
+},[socketRef.current]);   
+
   return <textarea id='realTimeEditor'></textarea>;
 };
 
-export default Editor; 
+export default Editor;  
